161d30c6a2e5e615fed05d985e08489a2403aa7c
Nila Chandra Laishram <nilalaishram@gmail.com>
2024-06-30 23:30:25 +0530

    mvp build ship

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -3,10 +3,11 @@ import { NextResponse } from 'next/server'
 import { type CookieOptions, createServerClient } from '@supabase/ssr'
 
 export async function GET(request: Request) {
-  const { searchParams, origin } = new URL(request.url)
+  const { searchParams } = new URL(request.url)
   const code = searchParams.get('code')
   // if "next" is in param, use it as the redirect URL
   const next = searchParams.get('next') ?? '/'
+  const customDomain = process.env.NEXT_PUBLIC_CUSTOM_DOMAIN! // Use your environment variable
 
   if (code) {
     const cookieStore = cookies()
@@ -29,10 +30,10 @@ export async function GET(request: Request) {
     )
     const { error } = await supabase.auth.exchangeCodeForSession(code)
     if (!error) {
-      return NextResponse.redirect(`${origin}${next}`)
+      return NextResponse.redirect(`${customDomain}${next}`)
     }
   }
 
   // return the user to an error page with instructions
-  return NextResponse.redirect(`${origin}/auth/auth-code-error`)
+  return NextResponse.redirect(`${customDomain}/auth/auth-code-error`)
 }
